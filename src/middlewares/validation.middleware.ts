28a5c6c15c262f09392b8578dc748c8dc058291a
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2025-04-20 16:17:38 -0300

    feat(middleware): configure validation middleware

---
+++ src/middlewares/validation.middleware.ts
@@ -0,0 +1,29 @@
+import { NextFunction, Request, Response } from "express";
+import { taskSchema } from "../schemas/taskSchema";
+import { noteSchema } from "../schemas/noteSchema";
+
+export function validateTask(req: Request, res: Response, next: NextFunction) {
+  const validation = taskSchema.safeParse(req.body);
+
+  if (!validation.success) {
+    const errosDetails = validation.error.errors.map(
+      (detail) => detail.message,
+    );
+    return res.status(400).json({ erros: errosDetails });
+  }
+
+  next();
+}
+
+export function validateNote(req: Request, res: Response, next: NextFunction) {
+  const validation = noteSchema.safeParse(req.body);
+
+  if (!validation.success) {
+    const errosDetails = validation.error.errors.map(
+      (detail) => detail.message,
+    );
+    return res.status(400).json({ errors: errosDetails });
+  }
+
+  next();
+}
