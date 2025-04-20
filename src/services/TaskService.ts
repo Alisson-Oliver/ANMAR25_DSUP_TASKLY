d5bf8b7934eae96519d4956b66817490f907284c
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2025-04-20 18:12:06 -0300

    feat(service): add getById task logic

--- src/services/TaskService.ts
+++ src/services/TaskService.ts
@@ -5,6 +5,15 @@ class TaskService {
   async create(date: Partial<Task>) {
     return await TaskRepository.create(date);
   }
+
+  async findById(id: number) {
+    const task = await TaskRepository.findById(id);
+    if (!task) {
+      throw new Error("task not found");
+    }
+
+    return task;
+  }
 }
 
 export default new TaskService();
