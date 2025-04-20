590140eac0fa60f3fa551f39f4b8d515f2b3b001
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2025-04-20 18:13:05 -0300

    feat(route): add GET /tasks/:id endpoint

--- src/routes/task.routes.ts
+++ src/routes/task.routes.ts
@@ -4,6 +4,7 @@ import { validateTask } from "../middlewares/validation.middleware";
 
 const router = Router();
 
+router.get("/tasks/:id", TaskController.findById);
 router.post("/tasks", validateTask, TaskController.create);
 
 export default router;
