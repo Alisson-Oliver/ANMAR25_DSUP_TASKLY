9a64c01369f56c44925c6d939b550a78ac56327e
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2025-04-22 15:19:35 -0300

    feat(schema): add note validation scheme

--- src/schemas/noteSchema.ts
+++ src/schemas/noteSchema.ts
@@ -1,8 +1,16 @@
 import { z } from "zod";
 
-export const noteSchema = z.object({
+export const noteSchemaAdd = z.object({
   content: z
     .string({ required_error: "'content' is required" })
     .min(1, "'content' cannot be empty")
     .max(150, "'content' must be at most 150 characters"),
+});
+
+export const noteSchemaUpdate = z.object({
+  content: z
+    .string()
+    .min(1, "'content' cannot be empty")
+    .max(150, "'content' must be at most 150 characters")
+    .optional(),
 });
