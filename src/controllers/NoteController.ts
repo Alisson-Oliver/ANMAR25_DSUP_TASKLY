b86913d8525f8ae722c10300ca15bfca13e8d24a
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2025-04-22 15:12:14 -0300

    feat(controller): add findByIdTask note handler

--- src/controllers/NoteController.ts
+++ src/controllers/NoteController.ts
@@ -2,6 +2,39 @@ import { Request, Response } from "express";
 import NoteService from "../services/NoteService.js";
 
 class NoteController {
+  async findAll(req: Request, res: Response) {
+    try {
+      const notes = await NoteService.findAll();
+      res.status(200).json({ data: { notes } });
+    } catch (error) {
+      if (error instanceof Error) {
+        res.status(500).json({ error: error.message });
+      }
+    }
+  }
+
+  async findByIdTask(req: Request, res: Response) {
+    try {
+      const { taskId } = req.params;
+      const taskIdNumber = Number(taskId);
+
+      if (!taskIdNumber || isNaN(Number(taskIdNumber))) {
+        res.status(400).json({ error: "id is required" });
+        return;
+      }
+
+      const notes = await NoteService.findByIdTask(taskIdNumber);
+      res.status(200).json({ data: { notes } });
+    } catch (error) {
+      if (error instanceof Error) {
+        if (error.message === "note not found") {
+          res.status(404).json({ error: error.message });
+        }
+        res.status(500).json({ error: error.message });
+      }
+    }
+  }
+
   async create(req: Request, res: Response): Promise<void> {
     try {
       const data = req.body;
