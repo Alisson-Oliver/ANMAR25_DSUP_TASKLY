3e101a9027dddf20ba812b0de39f8cb9b2ab37d6
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2025-04-22 15:24:07 -0300

    feat(route): add DELETE /notes/:id endpoint

--- src/routes/note.routes.ts
+++ src/routes/note.routes.ts
@@ -11,5 +11,6 @@ router.get("/tasks/:taskId/notes", NoteController.findByIdTask);
 router.post("/tasks/:taskId/notes", validateNoteAdd, NoteController.create);
 router.get("/notes/:id", NoteController.findById);
 router.put("/notes/:id", validateNoteUpdate, NoteController.update);
+router.delete("/notes/:id", NoteController.delete);
 
 export default router;
