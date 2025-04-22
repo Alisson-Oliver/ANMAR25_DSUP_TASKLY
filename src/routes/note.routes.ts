22b5234105e66de0f8f93ee63a70f73085b06d81
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2025-04-22 15:12:46 -0300

    feat(route): add GET /tasks/:taskId/notes endpoint

--- src/routes/note.routes.ts
+++ src/routes/note.routes.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { validateNoteAdd } from "../middlewares/validation.middleware.js";
+import {
+  validateNoteAdd,
+  validateNoteUpdate,
+} from "../middlewares/validation.middleware.js";
 import NoteController from "../controllers/NoteController.js";
 
 const router = Router();
 
+router.get("/tasks/:taskId/notes", NoteController.findByIdTask);
 router.post("/tasks/:taskId/notes", validateNoteAdd, NoteController.create);
 
 export default router;
