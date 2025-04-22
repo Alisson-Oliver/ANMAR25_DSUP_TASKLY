40dc03b2f7c9748771608420bfea4b4489dd36a0
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2025-04-22 15:21:16 -0300

    feat(route): add PUT /notes/:id endpoint

--- src/routes/note.routes.ts
+++ src/routes/note.routes.ts
@@ -1,5 +1,8 @@
 import { Router } from "express";
-import { validateNoteAdd } from "../middlewares/validation.middleware.js";
+import {
+  validateNoteAdd,
+  validateNoteUpdate,
+} from "../middlewares/validation.middleware.js";
 import NoteController from "../controllers/NoteController.js";
 
 const router = Router();
@@ -7,5 +10,6 @@ const router = Router();
 router.get("/tasks/:taskId/notes", NoteController.findByIdTask);
 router.post("/tasks/:taskId/notes", validateNoteAdd, NoteController.create);
 router.get("/notes/:id", NoteController.findById);
+router.put("/notes/:id", validateNoteUpdate, NoteController.update);
 
 export default router;
