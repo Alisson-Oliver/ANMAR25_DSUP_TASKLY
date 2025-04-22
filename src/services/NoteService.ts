436a2669f006a3c43e9fbcc2f2770fa7b7d5e893
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2025-04-22 15:11:48 -0300

    feat(service): add findByTaskId note logic

--- src/services/NoteService.ts
+++ src/services/NoteService.ts
@@ -3,6 +3,19 @@ import NoteRepository from "../repositories/NoteRepository.js";
 import TaskRepository from "../repositories/TaskRepository.js";
 
 class NoteService {
+  async findAll() {
+    return await NoteRepository.getAll();
+  }
+
+  async findByIdTask(taskId: number) {
+    const existingTask = await TaskRepository.findById(taskId);
+
+    if (!existingTask) {
+      throw new Error("note not found");
+    }
+    return await NoteRepository.getByIdTask(taskId);
+  }
+
   async create(data: Note) {
     const taskId = data.task.id;
     const existingTask = await TaskRepository.findById(taskId);
